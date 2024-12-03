6ca2d2bd7646637ac676142e40a14d98409e615e
Göksu Ayaz <goksuayaz79@gmail.com>
2024-12-03 13:54:45 +0300

    LoginPage

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Login from './Components/Login/Login';
+
+function App() {
+  return (
+    <div>
+
+      <Login />
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Login.css';
+
+const LoginSignup = () => {
+    return (
+        <div className='container'>
+            <div className='header'>
+                <div className='text'>techcareer.net'e Giriş Yap</div>
+            </div>
+
+            <div className='social-media'>
+                <text>Github</text>
+                <text>Google</text>
+            </div>
+
+            <div className='inputs'>
+                <div className='input'>
+                    <input type='email' placeholder='Email' />
+                </div>
+                <div className='input'>
+                    <input type='password' placeholder='Password' />
+                </div>
+            </div>
+
+            <div className="submit-container">
+                <div className="submit">Login</div>
+            </div>
+
+            <div className="forgot-password">
+                Lost Password? <span>Click Here!</span>
+            </div>
+        </div>
+    );
+};
+
+export default LoginSignup;
